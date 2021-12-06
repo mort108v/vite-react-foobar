524a7f58dc9dd1f7e19f56b7b5beb5d31d1a589d
Morten Six <mort108v@stud.kea.dk>
2021-12-06 21:28:18 +0100

    SCSS moved to src

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "../scss/main.scss";
+import "./SCSS/main.scss";
 import App from "./App";
 import "font-awesome/css/font-awesome.min.css";
 
